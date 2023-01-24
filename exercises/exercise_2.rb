f9afc96709514d8e900b1a05af1da0e34377b874
larsyaeger <larsyaeger@gmail.com>
2023-01-23 21:39:13 -0600

    completed the second exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+puts @store1.name
+puts @store2.name
+@store1.name = 'Victoria'
+puts @store1.name
